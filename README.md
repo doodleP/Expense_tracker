43b3d0d316a84389735cd630e093153c979625c4
doodleP <shreyakoshta205@gmail.com>
2025-04-06 17:10:03 +0530

    added UI

--- README.md
+++ README.md
@@ -1,17 +1,24 @@
 # Expense Tracker
 
 ## Description
-The Expense Tracker is a Python-based application that helps users manage their expenses efficiently. It uses a MySQL database to store transaction records, allowing users to add, view, and delete expenses. This project is useful for tracking spending habits and managing personal finances.
+The Expense Tracker is a Python-based web application that helps users manage their expenses efficiently. It uses Flask for UI and a MySQL database to store transaction records, allowing users to add and view expenses. This project is useful for tracking spending habits and managing personal finances.
 
 ## Features
 - Add new expenses with details such as amount, category, and date.
 - View a list of all recorded expenses.
-- Delete specific expenses from the database.
 - Secure and structured data storage using MySQL.
+- clean UI made with Flask
 
 ## Technologies Used
-- **Programming Language:** Python
-- **Database:** MySQL
-- **Libraries:**
-  - `mysql-connector-python` (for database connection)
-  - `tabulate` (for displaying tabular data)
+- **Programming Language:** 
+  - Python
+  - HTML
+  - CSS
+- **Database:** 
+  - MySQL
+- **Libraries/Frameworks:**
+  - MySQL
+  - Flask 
+  - pandas
+  - python-dotenv
+  - os (Python built-in)
---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, url_for, request, redirect
+import mysql.connector
+from dotenv import load_dotenv
+import os
+import pandas as pd
+
+load_dotenv()
+
+app = Flask(__name__)
+
+conn = mysql.connector.connect(
+    host = os.getenv('DB_HOST'),
+    user = os.getenv('DB_USER'),
+    password = os.getenv('DB_PASSWORD'),
+    database = os.getenv('DB_NAME')
+)
+cursor = conn.cursor()
+
+@app.route('/')          #homepage
+
+def index():
+    cursor.execute("SELECT * FROM expense")
+    records = cursor.fetchall()
+    
+    df = pd.DataFrame(records, columns = ['ID', 'Date', 'Category', 'Amount', 'Description'])
+    table = df.to_html(classes='table table-striped', index=False, border=0, justify='center')
+
+    return render_template("index.html", table = table)
+
+@app.route('/add_expense', methods=['POST'])    #add expense 
+def add_entry():
+    date = request.form['date']
+    category = request.form['category']
+    amount = request.form['amount']
+    description = request.form['description']
+
+    if description.strip() == '':
+        cursor.execute("INSERT INTO expense (date, category, amount, description) VALUES (%s, %s, %s, NULL)", (date, category, amount))
+    else:
+        cursor.execute("INSERT INTO expense (date, category, amount, description) VALUES (%s, %s, %s, %s)", (date, category, amount, description))
+    conn.commit()
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:expense_id>')     #delete expense 
+def delete_entry(expense_id):
+    cursor.execute("DELETE FROM expense WHERE id = %s", (expense_id,))
+    conn.commit()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
